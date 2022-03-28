7f4948180436a4aca2bd1a9600982b80656fbdbd
naeunchan <eunchan0492@naver.com>
2022-03-28 14:05:20 +0900

    update profile

--- src/components/App.js
+++ src/components/App.js
@@ -5,23 +5,43 @@ import { useEffect } from "react";
 
 const App = () => {
     const [init, setInit] = useState(false);
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
     const [userObj, setUserObj] = useState(null);
 
     useEffect(() => {
         authService.onAuthStateChanged((user) => {
             if (user) {
-                setIsLoggedIn(true);
-                setUserObj(user);
+                setUserObj({
+                    displayName: user.displayName,
+                    uid: user.uid,
+                    updateProfile: (args) => user.updateProfile(args),
+                });
             } else {
-                setIsLoggedIn(false);
+                setUserObj(null);
             }
 
             setInit(true);
         });
     }, []);
 
-    return { init } ? <Router isLoggedIn={isLoggedIn} userObj={userObj} /> : "Initializing";
+    const refreshUser = () => {
+        const user = authService.currentUser;
+
+        setUserObj({
+            displayName: user.displayName,
+            uid: user.uid,
+            updateProfile: (args) => user.updateProfile(args),
+        });
+    };
+
+    return (
+        <>
+            {init ? (
+                <Router refreshUser={refreshUser} isLoggedIn={Boolean(userObj)} userObj={userObj} />
+            ) : (
+                "Initializing..."
+            )}
+        </>
+    );
 };
 
 export default App;
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,16 +1,40 @@
 import { authService } from "myBase";
-import React from "react";
+import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 
-const Profile = () => {
+const Profile = ({ refreshUser, userObj }) => {
     const history = useHistory();
+    const [newDisplayName, setNewDisplayName] = useState(userObj.displayName);
+
     const handleLogOutClick = () => {
         authService.signOut();
         history.push("/);");
     };
 
+    const handleChange = (e) => {
+        const { value } = e.target;
+
+        setNewDisplayName(value);
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        if (userObj.displayName !== newDisplayName) {
+            await userObj.updateProfile({
+                displayName: newDisplayName,
+            });
+
+            refreshUser();
+        }
+    };
+
     return (
         <>
+            <form onSubmit={handleSubmit}>
+                <input type="text" placeholder="Enter your new name!" onChange={handleChange} />
+                <input type="submit" value="Update Profile" />
+            </form>
             <button onClick={handleLogOutClick}>Log Out</button>
         </>
     );
